af70dc23aee094cbec2a309d344f22fa038bd6ca
Prem <premkamban@gmail.com>
2020-04-11 23:34:19 +0530

    Added order button fucntionality

--- client/src/containers/Builder/BuildControls.js
+++ client/src/containers/Builder/BuildControls.js
@@ -11,15 +11,22 @@ const control = [
 
 const BuildControls = (props) => (
   <div className="BuildControls">
+    <p>
+      Current Price: <strong>{props.price}</strong>
+    </p>
     {control.map((ct) => {
       return (
         <BuildControl
           label={ct.label}
           key={ct.label}
+          remove={() => props.incrRemove(ct.type)}
           add={() => props.incredientsAdded(ct.type)}
         />
       );
     })}
+    <button className="OrderButton" disabled={!props.purchaseAble}>
+      Order Now!
+    </button>
   </div>
 );
 
--- client/src/containers/Builder/Builder.js
+++ client/src/containers/Builder/Builder.js
@@ -21,13 +21,26 @@ export default class Builder extends Component {
         meat: 0,
       },
       totalPrice: 4,
+      purchaseAble: false,
     };
     this.addIncredient = this.addIncredient.bind(this);
+    this.removeIncredient = this.removeIncredient.bind(this);
+  }
+
+  updatePurchaseState(incredients) {
+    const sum = Object.keys(incredients)
+      .map((ikey) => {
+        return incredients[ikey];
+      })
+      .reduce((sum, el) => {
+        //console.log(`The sum of ${type} `, sum);
+        return sum + el;
+      }, 0);
+
+    this.setState({ purchaseAble: sum > 0 });
   }
 
   addIncredient(type) {
-    console.log("what type is it ====>", type);
-    console.log("Incredent Type", this.state);
     const OldCount = this.state.incredients[type];
     const updateCount = OldCount + 1;
     const updatedState = {
@@ -38,15 +51,37 @@ export default class Builder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + costAddition;
     this.setState({ incredients: updatedState, totalPrice: newPrice });
+    this.updatePurchaseState(updatedState);
   }
 
-  removeIncredient() {}
+  removeIncredient(type) {
+    const OldCount = this.state.incredients[type];
+    if (OldCount <= 0) {
+      alert(`No ${type} incredient Added!`);
+      return;
+    }
+    const updateCount = OldCount - 1;
+    const updatedState = {
+      ...this.state.incredients,
+    };
+    updatedState[type] = updateCount;
+    const costAddition = INCRE_PRICE[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - costAddition;
+    this.setState({ incredients: updatedState, totalPrice: newPrice });
+    this.updatePurchaseState(updatedState);
+  }
 
   render() {
     return (
       <Aux>
         <Burger incredients={this.state.incredients} />
-        <BuildControls incredientsAdded={this.addIncredient} />
+        <BuildControls
+          incrRemove={this.removeIncredient}
+          incredientsAdded={this.addIncredient}
+          price={this.state.totalPrice}
+          purchaseAble={this.state.purchaseAble}
+        />
       </Aux>
     );
   }
